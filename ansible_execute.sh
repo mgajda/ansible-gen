0241cc516b45dce8ba5e5ecf61ea83195235dee1
root <root@vpntesttool2>
2017-07-26 18:57:44 -0400

    setup notes

--- ansible_execute.sh
+++ ansible_execute.sh
@@ -1,1 +1,2 @@
-ansible-playbook -i hosts ansible.yml --private-key=mhassan.pem
+#ansible-playbook -i hosts ansible.yml --private-key=$key
+ansible-playbook -i ec2.py  ansible.yml --private-key=mhassan.pem
